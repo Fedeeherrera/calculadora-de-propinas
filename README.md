6c64112b8d7590449b9c1c05ff443dfc569e2e96
Fedeeherrera <Fedeeherrera2001@gmail.com>
2024-06-04 21:47:21 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Calculadora de propinas 
+
+#### Utilice TSX y Tailwind para los estilos. Tambien Vite para la inicializacion del proyecto.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-4xl font-black text-center">
+          calculadora de Propinas
+        </h1>
+      </header>
+    </>
+  )
+}
+
+export default App
