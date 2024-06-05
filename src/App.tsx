882f4056eb61a97a6e616a05e6fb62f67f058ce9
Federico Herrera Modernizacion <fedeeherrera2001@gmail.com>
2024-06-05 12:44:22 -0300

    creacion de MenuItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { menuItems } from "./data/db"
+import MenuItem from './components/MenuItem'
+import { menuItems } from './data/db'
 
 function App() {
-
   console.log(menuItems)
 
   return (
@@ -11,6 +11,20 @@ function App() {
           calculadora de Propinas
         </h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className='p-5'>
+          <h2 className='text-4xl font-black'>Menu</h2>
+          <div className='space-y-3  mt-10'>
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   )
 }
